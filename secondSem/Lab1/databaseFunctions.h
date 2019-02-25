726e25298d23ced6b982d77fdcbb3f214fc06e42
Ilya <illia.gide@gmail.com>
2019-02-26 00:30:38 +0200

    /

--- secondSem/Lab1/databaseFunctions.h
+++ secondSem/Lab1/databaseFunctions.h
@@ -230,7 +230,7 @@ void deleteMonster(std::vector <Monster> &monsters,  const std::string &name){
 }
 
 
-void modifyMonster(std::vector <Monster> &monsters, std::string name){
+void modifyMonster(std::vector <Monster> &monsters, const std::string &name){
 
     for (auto& i : monsters)
         if (strcmp(i.name, name.c_str()) == 0) {
@@ -355,4 +355,6 @@ TextTable findByTime(std::vector <Monster> &monsters, struct tm &lhs, struct tm 
 
     return t;
 }
+
+
 
